b7114a2231d8d29a4865c26a9a397b05802a3454
armanghazaryan12-byte <arman.ghazaryan12@tumo.org>
2026-07-17 14:12:00 +0400

    test

--- audioPlayer_Gagik/audio.js
+++ audioPlayer_Gagik/audio.js
@@ -1,142 +1,287 @@
-let data = {
-    title: [
-        "Billie Eilish - everything i wanted",
-        "Debussy - Reverie",
-        "Freddie Mercury - Mamma",
-        "Nemra - Nare",
-        "Polo & Pan - Canopee",
-        "Tigran Hamasyan - Leaving Paris"
+let data={
+    title:[
+    "Billie Eilish - everything i wanted",
+    "Debussy - Reverie",
+    "Freddie Mercury - Mamma",
+    "Nemra - Nare",
+    "Polo & Pan - Canopee",
+    "Tigran Hamasyan - Leaving Paris"
     ],
-
-    song: [
-        "music/Billie Eilish - everything i wanted.mp3",
-        "music/Debussy Reverie.mp3",
-        "music/Freddie Mercury - Mamma (Bohemian Rhapsody).mp3",
-        "music/Nemra - Nare.mp3",
-        "music/polo-pan-canopee.mp3",
-        "music/Tigran Hamasyan Leaving Paris.mp3"
+    artist:[
+    "Billie Eilish",
+    "Claude Debussy",
+    "Freddie Mercury",
+    "Nemra",
+    "Polo & Pan",
+    "Tigran Hamasyan"
     ],
-
-    poster: [
-        "https://i.gifer.com/7d20.gif",
-        "https://media2.giphy.com/media/5xtDarmSceWsGyD4VeE/source.gif",
-        "https://cdn.dribbble.com/users/1242979/screenshots/7099165/music.gif",
-        "https://i.gifer.com/8RWM.gif",
-        "https://media2.giphy.com/media/Y0GyZQpjqafoatTvjB/giphy.gif",
-        "https://cdn.dribbble.com/users/1921422/screenshots/5511883/freddie.gif"
+    song:[
+    "music/Billie Eilish - everything i wanted.mp3",
+    "music/Debussy Reverie.mp3",
+    "music/Freddie Mercury - Mamma (Bohemian Rhapsody).mp3",
+    "music/Nemra - Nare.mp3",
+    "music/polo-pan-canopee.mp3",
+    "music/Tigran Hamasyan Leaving Paris.mp3"
+    ],
+    poster:[
+    "https://i.gifer.com/7d20.gif",
+    "https://media2.giphy.com/media/5xtDarmSceWsGyD4VeE/source.gif",
+    "https://media0.giphy.com/media/cPa0VPT5ZR8d6s9UGU/giphy.gif",
+    "https://media0.giphy.com/media/MDb7nlCn5mtcEZ8CtK/giphy.gif",
+    "https://media2.giphy.com/media/rh5R1rp1iIT8E22BBH/giphy.gif",
+    "https://cdn.dribbble.com/users/1921422/screenshots/5511883/freddie.gif"
     ]
-};
-
-let song = new Audio();
-let currentSong = 0;
-
-window.onload = function () {
-    playSong();
-};
-
-function playSong() {
-    song.src = data.song[currentSong];
-
-    let songTitle = document.getElementById("songTitle");
-    songTitle.textContent = data.title[currentSong];
-
-    let img = document.getElementById("row1");
-    img.style.backgroundImage = `url(${data.poster[currentSong]})`;
-
-    let main = document.getElementById("main");
-    main.style.backgroundImage = `url(${data.poster[currentSong]})`;
-
+    };
+    
+    let song=new Audio();
+    let currentSong=0;
+    let isPlaying=false;
+    
+    window.onload=function(){
+    loadSong();
+    };
+    
+    function loadSong(){
+    
+    song.src=data.song[currentSong];
+    
+    document.getElementById("songTitle").textContent=
+    data.title[currentSong];
+    
+    document.getElementById("artistName").textContent=
+    data.artist[currentSong];
+    
+    document.getElementById("coverImage").src=
+    data.poster[currentSong];
+    
+    document.getElementById("main").style.backgroundImage=
+    `url(${data.poster[currentSong]})`;
+    
+    }
+    
+    
+    function playOrPauseSong(){
+    
+    let play=document.getElementById("play");
+    let player=document.querySelector(".player");
+    
+    if(!isPlaying){
+    
     song.play();
-
-    let play = document.getElementById("play");
-    play.src = "images/pause.png";
-}
-
-function playOrPauseSong() {
-    let play = document.getElementById("play");
-
-    if (song.paused) {
-        song.play();
-        play.src = "images/pause.png";
-    } else {
-        song.pause();
-        play.src = "images/play-button-arrowhead.png";
+    
+    isPlaying=true;
+    
+    player.classList.add("playing");
+    
+    play.src="images/pause.png";
+    
+    }else{
+    
+    song.pause();
+    
+    isPlaying=false;
+    
+    player.classList.remove("playing");
+    
+    play.src="images/play-button-arrowhead.png";
+    
     }
-}
-
-song.addEventListener("timeupdate", function () {
-    let fill = document.getElementById("fill");
-
-    let position = song.currentTime / song.duration;
-    if (!isNaN(position)) {
-        fill.style.width = position * 100 + "%";
+    
     }
-
-    convertTime(song.currentTime);
-
-    if (song.ended) {
-        next();
-    }
-});
-
-function convertTime(seconds) {
-    let currentTime = document.getElementById("curentTime");
-
-    let min = Math.floor(seconds / 60);
-    let sec = Math.floor(seconds % 60);
-
-    min = (min < 10) ? "0" + min : min;
-    sec = (sec < 10) ? "0" + sec : sec;
-
-    currentTime.textContent = min + ":" + sec;
-
-    totalTime(Math.floor(song.duration));
-}
-
-function totalTime(seconds) {
-    if (isNaN(seconds)) return;
-
-    let currentTime = document.getElementById("curentTime");
-
-    let min = Math.floor(seconds / 60);
-    let sec = Math.floor(seconds % 60);
-
-    min = (min < 10) ? "0" + min : min;
-    sec = (sec < 10) ? "0" + sec : sec;
-
-    currentTime.textContent += " / " + min + ":" + sec;
-}
-
-function next() {
+    
+    
+    function next(){
+    
     currentSong++;
-
-    if (currentSong >= data.song.length) {
-        currentSong = 0;
+    
+    if(currentSong>=data.song.length){
+    
+    currentSong=0;
+    
     }
-
-    playSong();
-}
-
-function pre() {
+    
+    loadSong();
+    
+    playOrPauseSong();
+    
+    }
+    
+    
+    function pre(){
+    
     currentSong--;
-
-    if (currentSong < 0) {
-        currentSong = data.song.length - 1;
+    
+    if(currentSong<0){
+    
+    currentSong=data.song.length-1;
+    
     }
-
-    playSong();
     
-}
-
-
-function muted (){
-    let mute = document.getElementById("mute")
-if(song.muted){
-    song.muted = false
-    mute.src = "images volume.png"
-}else{
-    song.muted = true
-    mute.src = "images volume.png"
-}
+    loadSong();
     
-}
+    playOrPauseSong();
+    
+    }
+    
+    
+    song.addEventListener("timeupdate",function(){
+    
+    let fill=document.getElementById("fill");
+    let current=document.getElementById("currentTime");
+    let duration=document.getElementById("duration");
+    
+    
+    if(song.duration){
+    
+    fill.style.width=
+    (song.currentTime/song.duration)*100+"%";
+    
+    }
+    
+    
+    current.textContent=
+    formatTime(song.currentTime);
+    
+    duration.textContent=
+    formatTime(song.duration);
+    
+    });
+    
+    
+    function formatTime(time){
+    
+    if(isNaN(time)){
+    
+    return "00:00";
+    
+    }
+    
+    let min=Math.floor(time/60);
+    
+    let sec=Math.floor(time%60);
+    
+    
+    if(sec<10){
+    
+    sec="0"+sec;
+    
+    }
+    
+    if(min<10){
+    
+    min="0"+min;
+    
+    }
+    
+    
+    return min+":"+sec;
+    
+    }
+    
+    
+    
+    document.getElementById("volumeSlider")
+    .addEventListener("input",function(){
+    
+    song.volume=this.value/100;
+    
+    });
+    
+    
+    
+    function increase(){
+    
+    song.volume=Math.min(
+    1,
+    song.volume+0.1
+    );
+    
+    document.getElementById("volumeSlider").value=
+    song.volume*100;
+    
+    }
+    
+    
+    
+    function decrease(){
+    
+    song.volume=Math.max(
+    0,
+    song.volume-0.1
+    );
+    
+    document.getElementById("volumeSlider").value=
+    song.volume*100;
+    
+    }
+    
+    
+    
+    function muted(){
+    
+    let mute=document.getElementById("mute");
+    
+    song.muted=!song.muted;
+    
+    
+    if(song.muted){
+    
+    mute.src="images/mute.png";
+    
+    }else{
+    
+    mute.src="images/volume.png";
+    
+    }
+    
+    }
+    
+    
+    
+    document.getElementById("shuffle")
+    .onclick=function(){
+    
+    currentSong=
+    Math.floor(Math.random()*data.song.length);
+    
+    loadSong();
+    
+    playOrPauseSong();
+    
+    };
+    
+    
+    
+    document.getElementById("repeat")
+    .onclick=function(){
+    
+    song.loop=!song.loop;
+    
+    this.style.color=
+    song.loop ? "#ff00cc" : "white";
+    
+    };
+    
+    
+    
+    document.getElementById("heart")
+    .onclick=function(){
+    
+    this.innerHTML=
+    '<i class="fa-solid fa-heart"></i>';
+    
+    this.style.color="#ff0055";
+    
+    };
+    
+    
+    
+    song.addEventListener("ended",function(){
+    
+    if(!song.loop){
+    
+    next();
+    
+    }
+    
+    });
